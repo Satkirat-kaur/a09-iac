ad1b61f360090ae51a72709f32988c6c994356f4
Satkirat-kaur <kaur1852@algonquinlive.com>
2025-07-03 18:29:40 -0400

    Fix formatting issues

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
@@ -11,9 +11,9 @@ provider "azurerm" {
   features {}
 }
 
+
+
 resource "azurerm_resource_group" "example" {
   name     = "rg-cst8918-lab"
   location = "canadacentral"
 }
-
-# This is a test for GitHub Actions
